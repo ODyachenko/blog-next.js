1a47bb8eb762c7a4825c7d1c799837a72eedd3ce
ODyachenko <oleg1696@gmail.com>
2023-09-05 19:18:08 +0300

    Register form markup

--- components/Forms/RegisterForm.tsx
+++ components/Forms/RegisterForm.tsx
@@ -4,11 +4,13 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import './styles.scss';
 
 type Inputs = {
+  avatar: File;
+  fullName: string;
   email: string;
   password: string;
 };
 
-export const LoginForm: FC = () => {
+export const RegisterForm: FC = () => {
   const {
     register,
     handleSubmit,
@@ -22,7 +24,33 @@ export const LoginForm: FC = () => {
 
   return (
     <form className="login__form form" onSubmit={handleSubmit(onSubmit)}>
-      <h1 className="form__title">Log into the account</h1>
+      <h1 className="form__title">Сreate account</h1>
+      <label className="form__avatar">
+        <svg
+          fill="#bdbdbd"
+          width="100px"
+          height="100px"
+          viewBox="0 0 512 512"
+          id="_x30_1"
+          version="1.1"
+        >
+          <path d="M256,0C114.615,0,0,114.615,0,256s114.615,256,256,256s256-114.615,256-256S397.385,0,256,0z M256,90  c37.02,0,67.031,35.468,67.031,79.219S293.02,248.438,256,248.438s-67.031-35.468-67.031-79.219S218.98,90,256,90z M369.46,402  H142.54c-11.378,0-20.602-9.224-20.602-20.602C121.938,328.159,181.959,285,256,285s134.062,43.159,134.062,96.398  C390.062,392.776,380.839,402,369.46,402z" />
+        </svg>
+        <input type="file" accept="image/*" {...register('avatar')} />
+      </label>
+      <input
+        className="form__field"
+        type="text"
+        placeholder="Full name"
+        {...register('fullName', {
+          required: 'This field is required',
+          minLength: { value: 2, message: 'Min length is 2' },
+          maxLength: { value: 30, message: 'Max length is 30' },
+        })}
+      />
+      {errors.fullName && (
+        <span className="form__error">{errors.fullName.message}</span>
+      )}
       <input
         className="form__field"
         type="email"
