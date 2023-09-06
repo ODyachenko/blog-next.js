faf786365f86f17ebf3d73d644b06fc1648b25c8
ODyachenko <oleg1696@gmail.com>
2023-09-06 09:57:05 +0300

    Avatar preview for register form

--- components/Forms/RegisterForm.tsx
+++ components/Forms/RegisterForm.tsx
@@ -1,10 +1,11 @@
 'use client';
-import { FC } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { FC, useEffect, useState } from 'react';
+import Image from '@/node_modules/next/image';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 import './styles.scss';
 
 type Inputs = {
-  avatar: File;
+  avatar?: File;
   fullName: string;
   email: string;
   password: string;
@@ -15,28 +16,52 @@ export const RegisterForm: FC = () => {
     register,
     handleSubmit,
     reset,
+    control,
     formState: { errors },
   } = useForm<Inputs>({ mode: 'onChange' });
   const onSubmit: SubmitHandler<Inputs> = (data: Inputs) => {
     console.log(data);
     reset();
   };
+  const [files, setFiles] = useState<File[]>([]);
+  const urls = files.map((file) => URL.createObjectURL(file));
+
+  const onChangeField = (event: any) => {
+    const fileList = event.target.files;
+    fileList && setFiles([...fileList]);
+  };
 
   return (
     <form className="login__form form" onSubmit={handleSubmit(onSubmit)}>
       <h1 className="form__title">Сreate account</h1>
       <label className="form__avatar">
-        <svg
-          fill="#bdbdbd"
-          width="100px"
-          height="100px"
-          viewBox="0 0 512 512"
-          id="_x30_1"
-          version="1.1"
-        >
-          <path d="M256,0C114.615,0,0,114.615,0,256s114.615,256,256,256s256-114.615,256-256S397.385,0,256,0z M256,90  c37.02,0,67.031,35.468,67.031,79.219S293.02,248.438,256,248.438s-67.031-35.468-67.031-79.219S218.98,90,256,90z M369.46,402  H142.54c-11.378,0-20.602-9.224-20.602-20.602C121.938,328.159,181.959,285,256,285s134.062,43.159,134.062,96.398  C390.062,392.776,380.839,402,369.46,402z" />
-        </svg>
-        <input type="file" accept="image/*" {...register('avatar')} />
+        {!!files.length ? (
+          <Image
+            className="form__avatar--img"
+            src={urls[0]}
+            alt="test"
+            height={100}
+            width={100}
+          />
+        ) : (
+          <svg
+            fill="#bdbdbd"
+            width="100px"
+            height="100px"
+            viewBox="0 0 512 512"
+            id="_x30_1"
+            version="1.1"
+          >
+            <path d="M256,0C114.615,0,0,114.615,0,256s114.615,256,256,256s256-114.615,256-256S397.385,0,256,0z M256,90  c37.02,0,67.031,35.468,67.031,79.219S293.02,248.438,256,248.438s-67.031-35.468-67.031-79.219S218.98,90,256,90z M369.46,402  H142.54c-11.378,0-20.602-9.224-20.602-20.602C121.938,328.159,181.959,285,256,285s134.062,43.159,134.062,96.398  C390.062,392.776,380.839,402,369.46,402z" />
+          </svg>
+        )}
+
+        <input
+          type="file"
+          accept="image/*"
+          onChange={onChangeField}
+          //   {...register('avatar')}
+        />
       </label>
       <input
         className="form__field"
