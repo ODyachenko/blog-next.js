91462307273cdd397d99f34358107cb1493ef77e
ODyachenko <oleg1696@gmail.com>
2023-09-07 11:33:59 +0300

    fixed edit/delete post bug

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -22,40 +22,41 @@ export const Post: FC = ({
   const { isAuth } = useAppSelector((state: RootState) => state.user);
 
   return (
-    <Link href={`/posts/${_id}`} className="post">
-      <Image
-        className="post__cover"
-        src={cover}
-        alt={title}
-        width={690}
-        height={300}
-        priority
-      />
-
-      {isAuth && <PostActions id={_id} />}
-      <div className="post__footer">
+    <div className="post">
+      <Link href={`/posts/${_id}`}>
         <Image
-          className="post__avatar"
-          src={user.avatarUrl}
-          alt="Post avatar"
-          width={30}
-          height={30}
+          className="post__cover"
+          src={cover}
+          alt={title}
+          width={690}
+          height={300}
+          priority
         />
-        <div className="post__content">
-          <span className="post__author">{user.fullName}</span>
-          <span className="post__date">{convertDate(createdAt)}</span>
-          <h2 className="post__title">{title}</h2>
-          <PostTags tags={tags} />
-          <div className="post__stats">
-            <div className="post__stats-item">
-              <AiOutlineEye size={20} /> {viewsCount}
-            </div>
-            <div className="post__stats-item">
-              <LuMessageSquare size={20} /> {0}
+        <div className="post__footer">
+          <Image
+            className="post__avatar"
+            src={user.avatarUrl}
+            alt="Post avatar"
+            width={30}
+            height={30}
+          />
+          <div className="post__content">
+            <span className="post__author">{user.fullName}</span>
+            <span className="post__date">{convertDate(createdAt)}</span>
+            <h2 className="post__title">{title}</h2>
+            <PostTags tags={tags} />
+            <div className="post__stats">
+              <div className="post__stats-item">
+                <AiOutlineEye size={20} /> {viewsCount}
+              </div>
+              <div className="post__stats-item">
+                <LuMessageSquare size={20} /> {0}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+      {isAuth && <PostActions id={_id} />}
+    </div>
   );
 };
