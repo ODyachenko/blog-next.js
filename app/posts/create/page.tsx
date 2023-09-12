5e5e0f365ccb529e1f572fc23e30d4330c892465
ODyachenko <oleg1696@gmail.com>
2023-09-12 12:33:21 +0300

    Fixed build error

--- app/posts/create/page.tsx
+++ app/posts/create/page.tsx
@@ -1,21 +1,7 @@
-'use client';
 import { CreatePostForm } from '@/components/CreatePostForm/CreatePostForm';
 import { PageWrapper } from '@/components/PageWrapper/PageWrapper';
-import { useAppSelector } from '@/hooks/hooks';
-import { RootState } from '@/redux/store';
-import { useRouter } from 'next/navigation';
-import { ComponentState, useEffect } from 'react';
 
 export default function page() {
-  const { isAuth }: ComponentState = useAppSelector(
-    (state: RootState) => state.user
-  );
-  const router = useRouter();
-
-  useEffect(() => {
-    !isAuth && router.push('/');
-  }, [isAuth]);
-
   return (
     <PageWrapper>
       <CreatePostForm />
